89a39fb86224aec898bf8b3bb09e692b1ea3f575
Gagan Nandwana <nandwanagagan077@gmail.com>
2025-11-21 17:10:32 +0530

    TaskList Component

--- Taskman/src/App.jsx
+++ Taskman/src/App.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import { useState,useEffect } from 'react';
 import Taskform from './Components/Taskform'
 import Tasklist from './Components/Tasklist'
 import Progresstracker from './Components/Progresstracker'
 
 
 export default function App() {
-  const[tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
 
-  const addTask = (task) => {
-    setTasks(task);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
   }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = () => {
+
+  }
+
+
   return (
     <div>
       <header>
@@ -18,7 +35,7 @@ export default function App() {
         <p><i>Your friendly Task Manager</i></p>
       </header>
       <Taskform addTask = {addTask}/>
-      <Tasklist />
+      <Tasklist tasks = {tasks} updateTask = {updateTask} deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>clear All Tasks</button>
     </div>
